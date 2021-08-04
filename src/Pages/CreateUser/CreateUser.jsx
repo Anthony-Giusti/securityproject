4ffbf0914ca22eaf6369570bfbb38fd1ecc15944
Anthony Giusti <69980441+Anthony-Giusti@users.noreply.github.com>
2021-08-04 16:40:02 -0500

    Created a create user page currently empty

---
+++ src/Pages/CreateUser/CreateUser.jsx
@@ -0,0 +1,11 @@
+import { Typography } from '@material-ui/core';
+
+const CreateUser = () => {
+    const x = 2;
+
+    return( 
+        <div><Typography>Beep</Typography></div>
+    )
+}
+
+export default CreateUser;
