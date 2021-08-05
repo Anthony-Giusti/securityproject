4c4f8aeb557e043bad332fc8d79cb93492a58c61
Anthony Giusti <69980441+Anthony-Giusti@users.noreply.github.com>
2021-08-05 15:57:27 -0500

    Started work on create user page

--- src/Pages/CreateUser/CreateUser.jsx
+++ src/Pages/CreateUser/CreateUser.jsx
@@ -1,10 +1,50 @@
-import { Typography } from '@material-ui/core';
+import { FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@material-ui/core';
 
 const CreateUser = () => {
-    const x = 2;
+    let firstNameField;
+     let lastNameField;
 
     return( 
-        <div><Typography>Beep</Typography></div>
+        <div>
+            <Typography>New User</Typography>
+            <form>
+                <TextField
+                // error={firstNameError}
+                // onChange={editMade}
+                label="First Name"
+                // helperText={firstNameError ? 'Must contain only letters' : ''}
+                inputRef={(ref) => {
+                firstNameField = ref;
+                }}
+            />
+
+            <TextField
+                // error={lastNameError}
+                // onChange={editMade}
+                label="Last Name"
+                // helperText={lastNameError ? 'Must contain only letters' : ''}
+                inputRef={(ref) => {
+                lastNameField = ref;
+                }}
+            />
+
+            <FormControl>
+                <InputLabel>Sex</InputLabel>
+                <Select defaultValue={3}>
+                <MenuItem value={1}>M</MenuItem>
+                <MenuItem value={2}>F</MenuItem>
+                <MenuItem value={3}>NB</MenuItem>
+                </Select>
+            </FormControl>
+
+            {/* <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <BirthdayPicker
+                userBirthday={userBirthday}
+                userBirthdayChange={userBirthdayChange}
+                />
+            </MuiPickersUtilsProvider> */}
+            </form>
+        </div>
     )
 }
 
