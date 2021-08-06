5228f427292b348cc5e545ea2db8fd268ead762a
AlbatrossMissle <69980441+AlbatrossMissle@users.noreply.github.com>
2021-08-06 12:07:10 -0500

    Prettier changes. Sorting now works.

--- src/components/SortButton/SortButton.jsx
+++ src/components/SortButton/SortButton.jsx
@@ -3,34 +3,39 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import { Button, Divider, IconButton, Typography } from '@material-ui/core';
 
-const SortButton = ({handleSort, isCurrentlySelected, toBeSorted}) => {
-    const [currentDirection, setCurrentDirection] = useState('descending')
+const SortButton = ({ handleSort, toBeSorted, selectedSortBtn }) => {
+  const [currentDirection, setCurrentDirection] = useState('descending');
 
-    const handleClick = (direction) => {
-        handleSort(direction, toBeSorted)
+  const handleClick = (direction) => {
+    handleSort(direction, toBeSorted);
 
-        if (direction === 'ascending') {
-            setCurrentDirection('descending');
-        } else if (direction === 'descending') {
-            setCurrentDirection('ascending')
-        }
+    if (direction === 'ascending') {
+      setCurrentDirection('descending');
+    } else if (direction === 'descending') {
+      setCurrentDirection('ascending');
     }
+  };
 
-
-    return (
-        <>
-                {currentDirection === 'descending' &&( 
-                <IconButton onClick={() => handleClick('descending')}>
-                    <ExpandLessIcon />
-                </IconButton>)
-                }
-                {currentDirection === 'ascending' &&( 
-                <IconButton onClick={() => handleClick('ascending')}>
-                    <ExpandMoreIcon />
-                </IconButton>)
-                }
-        </>
-    )
-}
+  return (
+    <>
+      {currentDirection === 'descending' && (
+        <IconButton
+          color={selectedSortBtn === toBeSorted ? 'secondary' : 'default'}
+          onClick={() => handleClick('descending')}
+        >
+          <ExpandLessIcon id={toBeSorted} />
+        </IconButton>
+      )}
+      {currentDirection === 'ascending' && (
+        <IconButton
+          color={selectedSortBtn === toBeSorted ? 'secondary' : 'primary'}
+          onClick={() => handleClick('ascending')}
+        >
+          <ExpandMoreIcon id={toBeSorted} />
+        </IconButton>
+      )}
+    </>
+  );
+};
 
 export default SortButton;
--- src/components/Users/UsersHeader/UsersHeader.jsx
+++ src/components/Users/UsersHeader/UsersHeader.jsx
@@ -1,4 +1,4 @@
-import { Button, Divider, IconButton, Typography } from '@material-ui/core';
+import { Divider, IconButton, Typography } from '@material-ui/core';
 import React, { useState } from 'react';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
@@ -8,10 +8,16 @@ import UsersHeaderTools from './UsersHeaderTools/UsersHeaderTools';
 import useStyles from './Styles';
 import SortButton from '../../SortButton/SortButton';
 
-const UsersHeader = ({ handleSexFilter, sortUsers, sortByBirthday }) => {
+const UsersHeader = ({ handleSexFilter, sortUsers }) => {
   const [expandedHeader, setExpandedHeader] = useState(false);
-  const [currentSort, setCurrentSort] = useState(null);
+  const [selectedSortBtn, setSelectedSortBtn] = useState('id');
   const classes = useStyles();
+
+  const handleSort = (direction, toBeSorted) => {
+    sortUsers(direction, toBeSorted);
+
+    setSelectedSortBtn(toBeSorted);
+  };
 
   const handleExpandHeader = () => {
     setExpandedHeader((prev) => !prev);
@@ -22,35 +28,54 @@ const UsersHeader = ({ handleSexFilter, sortUsers, sortByBirthday }) => {
       <div className={classes.usersHeader}>
         <div>
           <Typography>ID</Typography>
-          <SortButton handleSort={sortUsers} toBeSorted='id'/>
+          <SortButton
+            handleSort={handleSort}
+            toBeSorted="id"
+            selectedSortBtn={selectedSortBtn}
+          />
         </div>
-  
+
         <Divider orientation="vertical" flexItem />
 
         <div>
           <Typography>First Name</Typography>
-          <SortButton handleSort={sortUsers} toBeSorted='firstName'/>
+          <SortButton
+            handleSort={handleSort}
+            toBeSorted="firstName"
+            selectedSortBtn={selectedSortBtn}
+          />
         </div>
 
         <Divider orientation="vertical" flexItem />
-        
+
         <div>
           <Typography>Last Name</Typography>
-          <SortButton handleSort={sortUsers} toBeSorted='lastName'/>
+          <SortButton
+            handleSort={handleSort}
+            toBeSorted="lastName"
+            selectedSortBtn={selectedSortBtn}
+          />
         </div>
 
         <Divider orientation="vertical" flexItem />
-        
+
         <div className={classes.sex}>
           <Typography>Sex</Typography>
         </div>
 
         <Divider orientation="vertical" flexItem />
+
         <div className={classes.birthday}>
           <Typography>Birthday</Typography>
-          <SortButton handleSort={sortUsers} toBeSorted='birthday'/>
+          <SortButton
+            handleSort={handleSort}
+            toBeSorted="birthday"
+            selectedSortBtn={selectedSortBtn}
+          />
         </div>
+
         <Divider orientation="vertical" flexItem />
+
         <IconButton onClick={handleExpandHeader}>
           {expandedHeader ? <ExpandLessIcon /> : <ExpandMoreIcon />}
         </IconButton>
