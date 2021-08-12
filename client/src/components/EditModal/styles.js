d1a7992b20b8dccdc0eba18084bf01ce9224eb27
Anthony Giusti <69980441+Anthony-Giusti@users.noreply.github.com>
2021-08-12 16:30:10 -0500

    Styles for edit modal

---
+++ client/src/components/EditModal/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core';
+import grey from '@material-ui/core/colors/grey';
+
+export default makeStyles((theme) => ({
+  userFields : {
+      display: 'flex',
+      flexWrap: 'wrap',
+  }
+}));
