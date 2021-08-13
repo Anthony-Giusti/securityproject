b2008439e9a9187ba713f575edd075d3300a8342
AlbatrossMissle <69980441+AlbatrossMissle@users.noreply.github.com>
2021-08-13 17:39:25 -0500

    Style changes. Calendar also will start on year selection

--- client/src/components/BirthdayPicker/BirthdayPicker.jsx
+++ client/src/components/BirthdayPicker/BirthdayPicker.jsx
@@ -36,12 +36,14 @@ const BirthdayPicker = ({
   return (
     <>
       <KeyboardDatePicker
+        inputVariant="outlined"
+        openTo="year"
         error={birthdayError}
         helperText={birthdayError ? 'Birthday must be in the past' : ''}
         disableToolbar
-        variant="inline"
+        variant="dialog"
         format="MM/dd/yyyy"
-        // margin="normal"
+        maxDate={new Date()}
         id="date-picker-inline"
         label="Date picker inline"
         value={userBirthday}
