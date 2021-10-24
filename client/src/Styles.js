f7f13bee557d386c67c12dbbf1f6ad2324de2cb2
AlbatrossMissle <69980441+AlbatrossMissle@users.noreply.github.com>
2021-10-24 15:44:17 -0500

    Will no longer be commiting Typescript generated files to repo

--- client/src/Styles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var core_1 = require("@material-ui/core");
-exports["default"] = core_1.makeStyles({
-    appMain: {
-        paddingTop: '5em'
-    }
-});
--- client/src/components/util/functions/createDateAndTimeString.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var date_and_time_1 = require("date-and-time");
-var createDateAndTimeString = function () {
-    return date_and_time_1["default"].format(new Date(), 'YYYY/MM/DD HH:mm:ss');
-};
-exports["default"] = createDateAndTimeString;
